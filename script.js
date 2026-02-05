eed66e243779121df8ece45834fe9fd66831f5b8
mileszh <mileszh05@gmail.com>
2026-02-05 00:26:21 -0500

    payment-03: Implement Luhn algorithm for card validation

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function handleFormSubmit(event) {
   event.preventDefault();
 
   const month = document.getElementById("month").value;
-  const year = document.getAnimations("year").value;
+  const year = document.getElementById("year").value;
   if (new Date() > new Date(year, month - 1)) {
     window.alert("Your card is expired!");
     return;
@@ -14,17 +14,36 @@ function handleFormSubmit(event) {
   const cvv = document.getElementById("cvv").value;
   if (!/^[0-9]{3,4}$/.test(cvv)) {
     window.alert("Invalid CVV. It must be 3 or 4 digits!");
-    returnl;
+    return;
   }
 
   const cnumber = document.getElementById("cnumber").value;
-  if (!isValid(cnumber)) {
-    return true;
+  if (!/^[0-9]{13,16}$/.test(cnumber) || !isValid(cnumber)) {
+    window.alert("Invalid card number!");
+    return;
   }
 
   window.alert("Thanks for the payment!");
 }
 
 function isValid(cnumber) {
-  return true;
+  let arr = cnumber
+    .split("")
+    .reverse()
+    .map((element) => parseInt(element));
+
+  const sum = arr.reduce(reducer, 0);
+
+  function reducer(accumulator, currentValue, currentIndex) {
+    currentIndex += 1;
+    if (currentIndex % 2 === 0) {
+      currentValue *= 2;
+      if (currentValue > 9) {
+        currentValue -= 9;
+      }
+    }
+    return accumulator + currentValue;
+  }
+
+  return sum % 10 === 0;
 }
